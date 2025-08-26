ce3ed3262362508c365ac9ffd5e88d2dd797b078
salman5698 <salmanali888000@gmail.com>
2025-08-26 22:47:42 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() 
+{
+    int num1,num2;
+    printf("enter first number =  ");
+    scanf("%d",&num1);
+     printf("enter second number =  ");
+    scanf("%d",&num2);
+    int sum =num1+num2;
+     printf("sum of two numbers is = %d",sum);
+    return 0;
+}
+
+
+
+
+
